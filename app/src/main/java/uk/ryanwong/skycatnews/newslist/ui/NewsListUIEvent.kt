fcb43f3d6037f048e7ae052c07c5f36f5a1e4411
Ryan Wong <ryan@rwmobi.com>
2023-04-09 16:01:36 +0100

    [refactor] NewsListScreen: move to keep ViewModel at NavGraph; create UIEvent data class

---
+++ app/src/main/java/uk/ryanwong/skycatnews/newslist/ui/NewsListUIEvent.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2023. Ryan Wong (hello@ryanwong.co.uk)
+ */
+
+package uk.ryanwong.skycatnews.uk.ryanwong.skycatnews.newslist.ui
+
+data class NewsListUIEvent(
+    val onStoryItemClicked: (id: Int) -> Unit,
+    val onWebLinkItemClicked: (id: Int) -> Unit,
+    val onRefresh: () -> Unit,
+    val onErrorShown: (errorId: Long) -> Unit,
+)
