9e5cd5b8d235bfbac35ff33553f8ad6d2606ed89
Ryan Wong <ryan@rwmobi.com>
2023-04-09 15:28:53 +0100

    [refactor] WebLinkScreen: create UIEvent and move UIState up from viewmodel package

---
+++ app/src/main/java/uk/ryanwong/skycatnews/weblink/ui/WebLinkUIEvent.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2023. Ryan Wong (hello@ryanwong.co.uk)
+ */
+
+package uk.ryanwong.skycatnews.uk.ryanwong.skycatnews.weblink.ui
+
+data class WebLinkUIEvent(
+    val onErrorShown: (errorId: Long) -> Unit,
+)
