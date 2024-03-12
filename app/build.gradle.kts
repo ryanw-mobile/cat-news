e81e971b9e50852dfeaa47eef3168f85708e11bf
Ryan Wong <ryan@rwmobi.com>
2024-03-12 17:19:27 +0000

    [chore] App: bump version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
         applicationId = "uk.ryanwong.catnews"
         minSdk = libs.versions.minsdk.get().toInt()
         targetSdk = libs.versions.targetsdk.get().toInt()
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "uk.ryanwong.catnews.app.ui.CustomTestRunner"
         vectorDrawables {
